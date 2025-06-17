79062458f60574640fa27668036012e4f031874b
matsumotonobushige <matsumoto.nobushige@nichicon.com>
2025-06-17 09:14:04 +0900

    modify two of urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,6 @@
+from django.urls import path
 from . import views
-from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
-    path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
